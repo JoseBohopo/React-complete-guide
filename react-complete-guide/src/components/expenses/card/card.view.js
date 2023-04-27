822c1cf778bee29640129a070e8e54e5f158b4d2
Jose Ramon Bohopo <joseramonbohopo@mbp-de-jose.home>
2023-04-27 12:40:09 +0200

    feat(card): add card component with styles and view

--- react-complete-guide/src/components/expenses/card/card.view.js
+++ react-complete-guide/src/components/expenses/card/card.view.js
@@ -1,5 +1,5 @@
-import "../ui/Card.css";
-export default function Card({ children, className }) {
+import "./card.styles.css";
+export function Card({ children, className }) {
   const CLASSES = "card " + className;
   return <div className={CLASSES}>{children}</div>;
 }
