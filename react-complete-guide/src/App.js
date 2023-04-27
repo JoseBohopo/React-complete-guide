529b2b37275764524fdf20224bb2e8e62ee863be
Jose Ramon Bohopo <joseramonbohopo@mbp-de-jose.home>
2023-04-27 12:49:23 +0200

    feat(App.js): add state for expenses and newExpenses, add yearPicker function to filter expenses by year, add addExpenseHandler function to add new expenses, pass newExpenses and yearPicker function as props to Expenses component, pass addExpenseHandler function as props to NewExpense component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,9 +1,52 @@
-import Expenses from "./components/expenses/Expenses";
+import { useState } from "react";
+import Expenses from "./components/expenses/index";
+import NewExpense from "./components/newExpense/index";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const [newExpenses, setNewExpenses] = useState(DUMMY_EXPENSES);
+
+  const yearPicker = (e) => {
+    e.target.value !== ""
+      ? setNewExpenses(
+          expenses.filter(
+            (expense) =>
+              Number(expense.date.getFullYear()) === Number(e.target.value)
+          )
+        )
+      : setNewExpenses(expenses);
+  };
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpense) => [expense, ...prevExpense]);
+  };
+
   return (
     <div>
-      <Expenses />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses expenses={newExpenses} onYearPicker={yearPicker} />
     </div>
   );
 }
