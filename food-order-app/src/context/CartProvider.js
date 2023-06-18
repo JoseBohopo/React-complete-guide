4a15cd30dd4c64c7438e46eaa3773c843247fae7
Jose Ramon Bohopo <joseramonbohopo@mbp-de-jose.home>
2023-06-18 18:37:29 +0200

    wire add and remove type in the reducer

--- food-order-app/src/context/CartProvider.js
+++ food-order-app/src/context/CartProvider.js
@@ -8,9 +8,28 @@ const defaultCartState = {
 
 const cartReducer = (state, actions) => {
   if (actions.type === "ADD") {
-    const updatedItems = state.items.concat(actions.item);
     const updatedAmount =
-      state.totalAmount + actions.item.price * actions.item.amount;
+      state.totalAmount + actions.item.price * +actions.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === actions.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: +existingCartItem?.amount + +actions.item?.amount,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(actions.item);
+    }
 
     return {
       items: updatedItems,
@@ -19,6 +38,26 @@ const cartReducer = (state, actions) => {
   }
 
   if (actions.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === actions.id
+    );
+
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - +existingItem.price;
+
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== actions.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
   return defaultCartState;
 };
